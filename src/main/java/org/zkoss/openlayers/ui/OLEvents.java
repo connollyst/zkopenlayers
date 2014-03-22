75624d6a4f130aad831c058256e39130896a445f
kubakudla <kuba.kudla@gmail.com>
2014-03-22 21:28:11 +0100

    more events added

--- src/main/java/org/zkoss/openlayers/ui/OLEvents.java
+++ src/main/java/org/zkoss/openlayers/ui/OLEvents.java
@@ -13,14 +13,24 @@ Copyright (C) 2012 Potix Corporation. All Rights Reserved.
 
 {{IS_RIGHT
 }}IS_RIGHT
-*/
+ */
 package org.zkoss.openlayers.ui;
 
 /**
  * @author jumperchen
- *
+ * @author Jakub Kudla (kubakudla)
+ * 
  */
 public class OLEvents {
 
 	public static final String ON_CHANGEBASELAYER = "onChangeBaseLayer";
+
+	public static final String ON_MOVE_END = "onMoveEnd";
+
+	public static final String ON_MOUSE_OUT = "onMouseOut";
+
+	public static final String ON_FEATURE_ADDED = "onFeatureAdded";
+
+	public static final String ON_FEATURE_SELECTED = "onFeatureSelected";
+
 }
